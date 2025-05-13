71257ee280d1404bca71289c3f3ee5fde6a7dfdb
Antonio Queb <cardstillo@gmail.com>
2025-05-12 21:53:02 -0600

    Up

--- models/service_order_line.py
+++ models/service_order_line.py
@@ -5,19 +5,36 @@ class ServiceOrderLine(models.Model):
     _description = 'Línea de Orden de Servicio'
 
     service_order_id = fields.Many2one(
-        'service.order', string='Orden de Servicio',
-        required=True, ondelete='cascade'
+        'service.order',
+        string='Orden de Servicio',
+        required=True,
+        ondelete='cascade'
     )
     product_id = fields.Many2one(
-        'product.product', string='Producto', required=True
+        'product.product',
+        string='Producto',
+        required=True
     )
     product_uom_qty = fields.Float(
-        string='Cantidad', default=1.0, required=True
+        string='Cantidad',
+        default=1.0,
+        required=True
     )
     product_uom = fields.Many2one(
-        'uom.uom', string='Unidad de Medida', required=True
+        'uom.uom',
+        string='Unidad de Medida',
+        required=True
+    )
+    packaging_id = fields.Many2one(
+        'product.packaging',
+        string='Embalaje de Producto',
+        help='Tipo de embalaje asociado al producto'
     )
     residue_type = fields.Selection(
-        [('rsu', 'RSU'), ('rme', 'RME'), ('rp', 'RP')],
-        string='Tipo de Residuos', required=False
+        [
+            ('rsu', 'RSU'),
+            ('rme', 'RME'),
+            ('rp', 'RP'),
+        ],
+        string='Tipo de Residuos'
     )
