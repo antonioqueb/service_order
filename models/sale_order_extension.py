b88a326fffcdc2b9c719cc310c763697b2f036cf
Antonio Queb <cardstillo@gmail.com>
2026-02-08 01:00:57 -0600

    feat: agregar campo para contar órdenes de servicio en la orden de venta

--- models/sale_order_extension.py
+++ models/sale_order_extension.py
@@ -8,6 +8,11 @@ _logger = logging.getLogger(__name__)
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    service_order_count = fields.Integer(
+        string="Órdenes de Servicio",
+        compute="_compute_service_order_count",
+    )
+
     def action_create_service_order(self):
         self.ensure_one()
 
